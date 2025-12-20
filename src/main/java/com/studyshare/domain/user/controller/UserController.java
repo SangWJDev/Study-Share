261cae4611c06ea561a06ea2cc29f5b358e68f46
woojin <tkdalsrb147@naver.com>
2025-12-20 17:51:22 +0900

    refactor: UserController 공통 응답 리팩토링

--- src/main/java/com/studyshare/domain/user/controller/UserController.java
+++ src/main/java/com/studyshare/domain/user/controller/UserController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.studyshare.domain.user.service.UserService;
-import com.studyshare.global.common.response.ApiResponse;
 import com.studyshare.domain.user.dto.LoginRequest;
 import com.studyshare.domain.user.dto.LoginResponse;
 import com.studyshare.domain.user.dto.SignupRequest;
 import com.studyshare.domain.user.dto.SignupResponse;
+import com.studyshare.domain.user.service.UserService;
+import com.studyshare.global.common.response.ApiResponse;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -26,14 +26,12 @@ public class UserController {
 
     @PostMapping("/signup")
     public ResponseEntity<ApiResponse<SignupResponse>> signup(@Valid @RequestBody SignupRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(ApiResponse.ok(userService.signup(request)));
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.ok(userService.signup(request)));
     }
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<LoginResponse>> login(@Valid @RequestBody LoginRequest request) {
-        return ResponseEntity.ok()
-                .body(ApiResponse.ok(userService.login(request)));
+        return ResponseEntity.ok().body(ApiResponse.ok(userService.login(request)));
     }
 
 }
